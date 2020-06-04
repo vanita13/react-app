5e93a2685a9b2e772221e65b441a217238dfc991
vanita Bhanushali <vidhibhanushali13@gmail.com>
2020-06-04 16:43:36 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import './App.css';
+import Menu from './components/MenuComponent';
 import {Navbar,NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href='/'>con fusion</NavbarBrand>
 
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
